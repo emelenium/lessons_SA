e1e2e2935bf3e6458d2a03722e46f6963b02f1aa
emelenium <emelenium@gmail.com>
2020-07-15 19:02:10 +0500

    урок 8 chekBox

--- p0081_ViewById/src/main/java/ru/education/develop/viewbyid/MainActivity.java
+++ p0081_ViewById/src/main/java/ru/education/develop/viewbyid/MainActivity.java
@@ -3,6 +3,8 @@ package ru.education.develop.viewbyid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main);
         TextView myTextView = (TextView) findViewById(R.id.myText);
         myTextView.setText("New text in TextView");
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
+        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
+        myChb.setChecked(true);
+
     }
 }
