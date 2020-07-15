488ac83065d7a5f7ef50dbfc7298258b4f6b52fa
emelenium <emelenium@gmail.com>
2020-07-15 19:47:53 +0500

    урок 9 OnClickListener

---
+++ p0091_OnClickButtons/src/main/java/ru/anton/education/develop/onclickbuttons/MainActivity.java
@@ -0,0 +1,40 @@
+package ru.anton.education.develop.onclickbuttons;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        // найдем View-элементы
+       tvOut = (TextView) findViewById(R.id.tvOut);
+       btnOk = (Button) findViewById(R.id.btnOk);
+       btnCancel = (Button) findViewById(R.id.btnCancel);
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+        // присвоим обработчик кнопке OK (btnOk)
+        btnOk.setOnClickListener(oclBtnOk);
+        btnCancel.setOnClickListener(oclBtnCancel);
+    }
+}
