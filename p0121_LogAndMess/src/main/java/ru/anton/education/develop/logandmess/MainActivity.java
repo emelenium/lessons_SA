8c9e815f7b9e13e62d790fd5f649a205ed380717
emelenium <emelenium@gmail.com>
2020-07-15 21:50:04 +0500

    урок 12 логи приложения

--- p0121_LogAndMess/src/main/java/ru/anton/education/develop/logandmess/MainActivity.java
+++ p0121_LogAndMess/src/main/java/ru/anton/education/develop/logandmess/MainActivity.java
@@ -3,12 +3,40 @@ package ru.anton.education.develop.logandmess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // найдем View-элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // присваиваем обработчик кнопкам
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // по id определяем кнопку, вызвавшую этот обработчик
+        switch (v.getId()) {
+            case R.id.btnOk:
+                // кнопка ОК
+                tvOut.setText("Нажата кнопка ОК");
+                break;
+            case R.id.btnCancel:
+                // кнопка Cancel
+                tvOut.setText("Нажата кнопка Cancel");
+                break;
+        }
     }
 }
