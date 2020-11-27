a8b3e6ec9974cf8796cc6644887f1a16be166987
emelenium <emelenium@gmail.com>
2020-11-27 17:13:02 +0500

    Урок 3: Приложение "Список" (Часть 1).

--- p0831_alishev_vk/src/main/java/ru/anton/education/develop/p0831_alishev_vk/MainActivity.java
+++ p0831_alishev_vk/src/main/java/ru/anton/education/develop/p0831_alishev_vk/MainActivity.java
@@ -3,12 +3,19 @@ package ru.anton.education.develop.p0831_alishev_vk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView plList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        findViewById(R.id.tv_pl_names);
+        String[] names = {"Java", "Python", "Ruby", "JavaScript", "Matlab"};
+        plList.setText("");
+        for(String name : names){
+            plList.append(name+"\n");
+        }
     }
 }
