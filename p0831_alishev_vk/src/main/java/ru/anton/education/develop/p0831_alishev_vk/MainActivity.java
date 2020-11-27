cb945c19531cb06324e49061edd716da00919703
emelenium <emelenium@gmail.com>
2020-11-27 15:04:01 +0500

    VK

---
+++ p0831_alishev_vk/src/main/java/ru/anton/education/develop/p0831_alishev_vk/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.anton.education.develop.p0831_alishev_vk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
